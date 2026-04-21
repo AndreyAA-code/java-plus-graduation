58ba725fc572e9d0ff71c3e4e348c389dd1756af
AndreyAA-code <otd53@yandex.ru>
2026-04-21 22:58:32 +0300

    создал user-service, добавил в конфиг и дискавери, начал переносить код

---
+++ core/user-service/src/main/java/ru/practicum/model/User.java
@@ -0,0 +1,36 @@
+package ru.practicum.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @Length(min = 6, max = 254)
+    @NotBlank
+    @Column
+    private String email;
+
+    @Length(min = 2, max = 250)
+    @NotBlank
+    @Column
+    private String name;
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", email=" + email + ", name=" + name + '}';
+    }
+}
